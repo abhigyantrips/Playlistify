a42b9ebe46a3b2c4e849e5322eeb97dc84c234ff
Abhigyan Trips <contact@abhigyantrips.dev>
2025-03-01 10:51:53 +0530

    feat(auth): init spotify auth and schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,4 @@
+import * as schema from '@/db/schema';
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
@@ -6,6 +7,7 @@ import { db } from '@/lib/db';
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
+    schema,
   }),
   socialProviders: {
     spotify: {
